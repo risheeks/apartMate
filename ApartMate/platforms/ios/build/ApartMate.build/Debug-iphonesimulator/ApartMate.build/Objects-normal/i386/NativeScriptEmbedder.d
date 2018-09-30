0d3d145580f06eced953843a2ee6fd458f1d8c92
adriangerardraj <34865898+adriangerardraj@users.noreply.github.com>
2018-09-30 12:56:30 -0400

    Deleting Nativescript stuff
    Removed all old NativeScript stuff

--- ApartMate/platforms/ios/build/ApartMate.build/Debug-iphonesimulator/ApartMate.build/Objects-normal/i386/NativeScriptEmbedder.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/SiD/Desktop/ApartMate/ApartMate/ApartMate/platforms/ios/ApartMate/ApartMate-Prefix.pch \
-  /Users/SiD/Desktop/ApartMate/ApartMate/ApartMate/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.m \
-  /Users/SiD/Desktop/ApartMate/ApartMate/ApartMate/platforms/ios/../../node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.h
