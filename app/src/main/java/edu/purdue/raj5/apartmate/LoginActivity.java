0eebbb75ecb758b2cc373fdec9b70f2ab6a2a81b
adriangerardraj <34865898+adriangerardraj@users.noreply.github.com>
2018-09-30 13:04:13 -0400

    Adding login/register pages and backend code
    Login and register buttons added. Able to extract email and password from the frontend page.
    
    TODO: Link to Firebase Database and finish Login and Register User Stories.

---
+++ app/src/main/java/edu/purdue/raj5/apartmate/LoginActivity.java
@@ -0,0 +1,62 @@
+package edu.purdue.raj5.apartmate;
+
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    Button login;
+    Button register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+       // login();
+        register();
+
+    }
+
+    private void register() {
+        email = (EditText)findViewById(R.id.et_email);
+        password = (EditText)findViewById(R.id.et_password);
+        login = (Button)findViewById(R.id.bt_register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),email.getText()+" "+password.getText(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void login()
+    {
+        email = (EditText)findViewById(R.id.et_email);
+        password = (EditText)findViewById(R.id.et_password);
+        login = (Button)findViewById(R.id.bt_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),email.getText()+" "+password.getText(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
+
