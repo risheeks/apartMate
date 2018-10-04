572a96eba864d64069a3ca580710b85379914d57
Akshay3398 <akshay3398@gmail.com>
2018-10-04 16:42:34 -0400

    Update MenuActivity.java

--- app/src/main/java/edu/purdue/raj5/MenuActivity.java
+++ app/src/main/java/edu/purdue/raj5/MenuActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 
@@ -12,18 +13,23 @@ public class MenuActivity extends AppCompatActivity {
 
     private ArrayList<String> mNames = new ArrayList<>();
     private ArrayList<String> mImages = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        intializeBitMaps();
+        Toolbar mToolBar = (Toolbar)findViewById(R.id.tb_menu);
+        setSupportActionBar(mToolBar);
+
+        initializeBitMaps();
+        initializeRecyclerView();
     }
 
-    private void intializeBitMaps(){
+    private void initializeBitMaps(){
         mImages.add("https://i.redd.it/tpsnoz5bzo501.jpg");
         mNames.add("Wassup fellas");
     }
-    private void intializeRecyclerView(){
+    private void initializeRecyclerView(){
         RecyclerView recyclerView = findViewById(R.id.rv_groupNames);
         MenuRecyclerViewAdaptor adaptor = new MenuRecyclerViewAdaptor(this, mNames, mImages);
         recyclerView.setAdapter(adaptor);
