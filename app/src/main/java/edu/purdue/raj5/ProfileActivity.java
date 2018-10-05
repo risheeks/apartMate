bbc0f94979176c23588572bc597a81847f03b2f0
Akshay3398 <akshay3398@gmail.com>
2018-10-05 00:11:46 -0400

    Change password and all the other textViews.

--- app/src/main/java/edu/purdue/raj5/ProfileActivity.java
+++ app/src/main/java/edu/purdue/raj5/ProfileActivity.java
@@ -9,8 +9,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -19,10 +24,78 @@ public class ProfileActivity extends AppCompatActivity {
     ImageButton ib_camera_profile;
     CircleImageView profilePhoto;
     Uri imageUri;
+    TextView tv_firstName;
+    TextView tv_lastName;
+    TextView tv_email;
+    TextView tv_LatestAachievement;
+    TextView tv_GreatestAchievement;
+    Button bt_changeProfile;
+    TextView tv_changePassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        initializeTextViews();
+        initializeButton();
+        takePhoto();
+        bt_changeProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
+                View view = LayoutInflater.from(ProfileActivity.this).inflate(R.layout.profile_editable,null);
+                final EditText et_firstName = (EditText) view.findViewById(R.id.et_profile_firstName);
+                final EditText et_lastName = (EditText) view.findViewById(R.id.et_profile_lastName);
+                final EditText et_latestAchievement = (EditText) view.findViewById(R.id.et_latestAchievement);
+                final EditText et_greatestAchievement = (EditText) view.findViewById(R.id.et_greatestAchievement);
+                Button bt_applyChanges = (Button) view.findViewById(R.id.bt_profileEdit);
+
+                bt_applyChanges.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(ProfileActivity.this, ""+et_firstName+" "+et_lastName+" "+et_latestAchievement+" "+et_greatestAchievement+" ", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.show();
+
+
+            }
+        });
+
+        tv_changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
+                View view = LayoutInflater.from(ProfileActivity.this).inflate(R.layout.password_change,null);
+                final EditText et_oldPassword = (EditText) view.findViewById(R.id.et_profile_firstName);
+                final EditText et_newPassword = (EditText) view.findViewById(R.id.et_profile_lastName);
+                Button bt_applyChanges = (Button) view.findViewById(R.id.bt_profileEdit);
+
+                bt_applyChanges.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(ProfileActivity.this, ""+et_oldPassword+" "+et_newPassword+" ", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.show();
+
+
+            }
+        });
+
+
+
+    }
+    private void initializeButton(){
+        bt_changeProfile = (Button)findViewById(R.id.bt_profileChenge);
+    }
+    private void initializeTextViews(){
+        tv_firstName = (TextView)findViewById(R.id.tv_profile_firstName);
+        tv_lastName = (TextView)findViewById(R.id.tv_profile_lastName);
+        tv_email = (TextView)findViewById(R.id.et_profile_email);
+        tv_LatestAachievement = (TextView)findViewById(R.id.tv_latestAchievement);
+        tv_GreatestAchievement = (TextView)findViewById(R.id.tv_greatestAchievement);
+        tv_changePassword = (TextView)findViewById(R.id.tv_changePassword);
     }
     private void takePhoto(){
         ib_camera_profile = (ImageButton)findViewById(R.id.ib_profilePhotoCamera);
@@ -34,6 +107,8 @@ public class ProfileActivity extends AppCompatActivity {
                 selectImage();
             }
         });
+
+
 
         /*
         ib_camera_profile.setOnClickListener(new View.OnClickListener() {
