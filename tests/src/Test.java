25d87951dfcb6fda0bf600d56a45129ba1bae2a5
Ian Rettig <irettig@purdue.edu>
2018-10-28 18:30:07 -0400

    Made CREATE_GROUP and ADD_GROUP Java test cases

--- tests/src/Test.java
+++ tests/src/Test.java
@@ -16,6 +16,10 @@ public class Test {
         test2_2();
         test2_3();
         Thread.sleep(1500);
+        test3();
+        Thread.sleep(1500);
+        test4_1();
+        test4_2();
     }
 
     //Adds user irettig
@@ -308,6 +312,174 @@ public class Test {
             @Override
             public void onConnect(Socket socket) throws IOException {
                 String message = "LOGIN corruptsoul13@gmail.com 12345";
+                c.send(message);
+
+            }
+
+            @Override
+            public void onDisconnect(Socket socket, String message) throws IOException {
+            }
+
+            @Override
+            public void onConnectError(Socket socket, String message) {
+            }
+        });
+
+        c.connect();
+    }
+
+    //create group test group 1
+    static void test3() {
+        Client c = new Client(ip, port);
+
+
+        c.setClientCallback(new Client.ClientCallback (){
+
+            @Override
+            public void onMessage(String message) {
+                //System.out.print("MESSAGE RECEIVED: " + message + "\n");
+
+                if(message.equals("LOGIN SUCCESS")) {
+                    String message2 = "CREATE_GROUP;irettig@purdue.edu;test group 1";
+                    c.send(message2);
+                }
+                else if (message.equals("CREATE_GROUP SUCCESS")) {
+                    System.out.print("Test: 3 Success\n");
+                    try {
+                        c.disconnect();
+                    } catch (java.io.IOException e) {
+                        System.out.print(e.getMessage());
+                    }
+                }
+                else if (message.equals("INVALID_COMMAND")) {
+                    System.out.print("Fix this Adrian\n");
+                }
+                else if (!message.equals("Welcome to the server!")) {
+                    System.out.print("Failed test 3: " + message + "\n");
+                    try {
+                        c.disconnect();
+                    } catch (java.io.IOException e) {
+                        System.out.print(e.getMessage());
+                    }
+                }
+
+            }
+
+            @Override
+            public void onConnect(Socket socket) throws IOException {
+                String message = "LOGIN irettig@purdue.edu 12345";
+                c.send(message);
+
+            }
+
+            @Override
+            public void onDisconnect(Socket socket, String message) throws IOException {
+            }
+
+            @Override
+            public void onConnectError(Socket socket, String message) {
+            }
+        });
+
+        c.connect();
+    }
+
+    //add raj5 to test group 1
+    static void test4_1() {
+        Client c = new Client(ip, port);
+
+
+        c.setClientCallback(new Client.ClientCallback (){
+
+            @Override
+            public void onMessage(String message) {
+                //System.out.print("MESSAGE RECEIVED: " + message + "\n");
+
+                if(message.equals("LOGIN SUCCESS")) {
+                    String message2 = "ADD_GROUP;raj5@purdue.edu;test group 1";
+                    c.send(message2);
+                }
+                else if (message.equals("ADD_GROUP SUCCESS")) {
+                    System.out.print("Test: 4_1 Success\n");
+                    try {
+                        c.disconnect();
+                    } catch (java.io.IOException e) {
+                        System.out.print(e.getMessage());
+                    }
+                }
+                else if (message.equals("INVALID_COMMAND")) {
+                    System.out.print("Fix this Adrian\n");
+                }
+                else if (!message.equals("Welcome to the server!")) {
+                    System.out.print("Failed test 4_1: " + message + "\n");
+                    try {
+                        c.disconnect();
+                    } catch (java.io.IOException e) {
+                        System.out.print(e.getMessage());
+                    }
+                }
+
+            }
+
+            @Override
+            public void onConnect(Socket socket) throws IOException {
+                String message = "LOGIN irettig@purdue.edu 12345";
+                c.send(message);
+
+            }
+
+            @Override
+            public void onDisconnect(Socket socket, String message) throws IOException {
+            }
+
+            @Override
+            public void onConnectError(Socket socket, String message) {
+            }
+        });
+
+        c.connect();
+    }
+
+    //add corruptsoul13 to test group 1
+    static void test4_2() {
+        Client c = new Client(ip, port);
+
+
+        c.setClientCallback(new Client.ClientCallback (){
+
+            @Override
+            public void onMessage(String message) {
+                //System.out.print("MESSAGE RECEIVED: " + message + "\n");
+
+                if(message.equals("LOGIN SUCCESS")) {
+                    String message2 = "ADD_GROUP;corruptsoul13@gmail.com;test group 1";
+                    c.send(message2);
+                }
+                else if (message.equals("ADD_GROUP SUCCESS")) {
+                    System.out.print("Test: 4_2 Success\n");
+                    try {
+                        c.disconnect();
+                    } catch (java.io.IOException e) {
+                        System.out.print(e.getMessage());
+                    }
+                }
+                else if (message.equals("INVALID_COMMAND")) {
+                    System.out.print("Fix this Adrian\n");
+                }
+                else if (!message.equals("Welcome to the server!")) {
+                    System.out.print("Failed test 4_2: " + message + "\n");
+                    try {
+                        c.disconnect();
+                    } catch (java.io.IOException e) {
+                        System.out.print(e.getMessage());
+                    }
+                }
+
+            }
+
+            @Override
+            public void onConnect(Socket socket) throws IOException {
+                String message = "LOGIN irettig@purdue.edu 12345";
                 c.send(message);
 
             }
