a5f2fa92c4ce5fed96844316cb0394556b0e0c39
Siddhant Patel <si12hant@gmail.com>
2018-11-02 17:11:29 -0400

    Doc InterestAdapter

--- app/src/main/java/edu/purdue/raj5/apartmate/InterestAdapter.java
+++ app/src/main/java/edu/purdue/raj5/apartmate/InterestAdapter.java
@@ -21,6 +21,11 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+/*
+* Adapter for the interest Class
+* Adds interest to fireBase
+*/
+
 public class InterestAdapter extends RecyclerView.Adapter<InterestAdapter.ViewHolder> {
     private  String groupName;
     private  String email;
@@ -69,6 +74,7 @@ public class InterestAdapter extends RecyclerView.Adapter<InterestAdapter.ViewHo
                         final FirebaseDatabase storage = FirebaseDatabase.getInstance();
                         final DatabaseReference storageRef = storage.getReference("Groups/" + groupName + "/Interests");
                         Log.e("GN",groupName);
+                        //FireBase call to get the groupName->interest and add the interest
                         storageRef.addValueEventListener(new ValueEventListener() {
                             @Override
                             public void onDataChange(DataSnapshot dataSnapshot) {
