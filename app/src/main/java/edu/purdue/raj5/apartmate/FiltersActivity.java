b105034c6d5642ae70ac18b862a0b210c48cdb54
RIsheek <risheek@pal-nat186-102-101.itap.purdue.edu>
2018-11-27 21:10:46 -0500

    Documentation
    documented the Filter Activity

--- app/src/main/java/edu/purdue/raj5/apartmate/FiltersActivity.java
+++ app/src/main/java/edu/purdue/raj5/apartmate/FiltersActivity.java
@@ -16,21 +16,23 @@ import org.w3c.dom.Text;
 public class FiltersActivity extends AppCompatActivity {
 
     //private MultiSelectionSpinner ageSpinner;
-    private MultiSelectionSpinner majorSpinner;
-    private RangeSeekBar<Integer> age;
-    private View btnSend;
-    private View btnCancel;
-    private EditText interests;
-    private EditText zip;
-    private TextView dispage;
-    Integer min = 18;
-    Integer max = 22;
+    private MultiSelectionSpinner majorSpinner; //major specifier
+    private RangeSeekBar<Integer> age; //a bar for picking age
+    private View btnSend; //a button to send
+    private View btnCancel; // a button to cancel
+    private EditText interests; // interests to match for
+    private EditText zip; // zip code to search potential roommates in
+    private TextView dispage; // The age range on the age seekbar
+    Integer min = 18; //default minimum age
+    Integer max = 22; //default maximum age
 
-    String[] agevalues = {"<18", "19", "20", "21", "22", ">22"};
+    //String[] agevalues = {"<18", "19", "20", "21", "22", ">22"};
+
+    //List of majors to pick from
     String[] majorvalues = {"Computer Science", "Engineering", "CGT", "Construction Management", "Philosophy", "English"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //on launching of this activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filters);
 
@@ -45,6 +47,20 @@ public class FiltersActivity extends AppCompatActivity {
         majorSpinner.setItems(majorvalues);
 
 
+        //the function called when the cancel button is clicked
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String myinterests = interests.getText().toString();
+                String majors = majorSpinner.getSelectedItemsAsString();
+                String myzip = zip.getText().toString();
+                Toast.makeText(FiltersActivity.this, myinterests + "\n" + myzip + "\n" + min + "-" + max + "\n" + majors, Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+
+        //the function called when the submit button is clicked
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,6 +73,7 @@ public class FiltersActivity extends AppCompatActivity {
             }
         });
 
+        //called whenever a chnage is made to the age seekbar
         age.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener<Integer>() {
             @Override
             public void onRangeSeekBarValuesChanged(RangeSeekBar<?> bar, Integer minValue, Integer maxValue) {
@@ -67,6 +84,7 @@ public class FiltersActivity extends AppCompatActivity {
             }
         });
 
+        //notifies while the ae seekbar is being dragged
         age.setNotifyWhileDragging(true);
 
     }
