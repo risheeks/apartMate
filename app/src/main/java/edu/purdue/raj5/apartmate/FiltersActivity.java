32af91449964c41f55b12de168353ea295bdda84
RIsheek <risheek@pal-nat186-102-101.itap.purdue.edu>
2018-11-27 23:12:36 -0500

    Themes for Filter
    Fixed the themes for the filters activity and added some peripheral frills!

--- app/src/main/java/edu/purdue/raj5/apartmate/FiltersActivity.java
+++ app/src/main/java/edu/purdue/raj5/apartmate/FiltersActivity.java
@@ -1,10 +1,16 @@
 package com.example.risheek.apartmate;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,26 +19,77 @@ import com.yahoo.mobile.client.android.util.rangeseekbar.RangeSeekBar;
 
 import org.w3c.dom.Text;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FiltersActivity extends AppCompatActivity {
 
     //private MultiSelectionSpinner ageSpinner;
-    private MultiSelectionSpinner majorSpinner; //major specifier
-    private RangeSeekBar<Integer> age; //a bar for picking age
-    private View btnSend; //a button to send
-    private View btnCancel; // a button to cancel
-    private EditText interests; // interests to match for
-    private EditText zip; // zip code to search potential roommates in
-    private TextView dispage; // The age range on the age seekbar
-    Integer min = 18; //default minimum age
-    Integer max = 22; //default maximum age
+    private MultiSelectionSpinner majorSpinner;
+    private RangeSeekBar<Integer> age;
+    private View btnSend;
+    private View btnCancel;
+    private EditText interests;
+    private EditText zip;
+    private TextView dispage;
+    private TextView interestL;
+    private TextView majorL;
+    private TextView dispageL;
+    private TextView zipL;
+    Integer min = 18;
+    Integer max = 22;
 
-    //String[] agevalues = {"<18", "19", "20", "21", "22", ">22"};
+    String[] agevalues = {"<18", "19", "20", "21", "22", ">22"};
+    String[] majorvalues = {"Computer Science", "Engineering", "CGT", "Construction Management", "Philosophy", "English", "Philosophy", "Business Management", "Accounting", "Communication Sciecnce", "Nursing", "History", "Math", "Economics", "Marketing", "Music", "Physics", "Political Science", "Theatre"};
 
-    //List of majors to pick from
-    String[] majorvalues = {"Computer Science", "Engineering", "CGT", "Construction Management", "Philosophy", "English"};
+    public void getAppTheme(String theme) { //theme is "light" or "dark"
+
+        //call this inside every activity
+        SharedPreferences preferences = this.getSharedPreferences("MyTheme", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("theme", theme);
+        editor.commit();
+//        interests = (EditText) findViewById(R.id.Interests);
+//        et_chatSearch = (TextView) findViewById(R.id.et_chatSearch);
+//        optionsButton = (ImageView) findViewById(R.id.iv_menuOptions);
+        LinearLayout ll = (LinearLayout) findViewById(R.id.filterll);
+        // The following code is used for theme preferences.
+        String s = preferences.getString("theme", "");
+        if (s.equals("dark")) {
+            ll.setBackgroundColor(Color.DKGRAY);
+            interests.setTextColor(Color.WHITE);
+            zip.setTextColor(Color.WHITE);
+            dispage.setTextColor(Color.WHITE);
+            majorSpinner.setBackgroundColor(Color.GRAY);
+            //age.setBackgroundColor(Color.WHITE);
+            interestL.setTextColor(Color.WHITE);
+            dispageL.setTextColor(Color.WHITE);
+            majorL.setTextColor(Color.WHITE);
+            zipL.setTextColor(Color.WHITE);
+            age.setBackgroundColor(Color.DKGRAY);
+
+        } else {
+            ll.setBackgroundColor(Color.WHITE);
+            interests.setTextColor(Color.BLACK);
+            zip.setTextColor(Color.BLACK);
+            dispage.setTextColor(Color.BLACK);
+            majorSpinner.setBackgroundColor(Color.WHITE);
+            //age.setBackgroundColor(Color.WHITE);
+            interestL.setTextColor(Color.BLACK);
+            dispageL.setTextColor(Color.BLACK);
+            majorL.setTextColor(Color.BLACK);
+            zipL.setTextColor(Color.BLACK);
+            age.setBackgroundColor(Color.WHITE);
+        }
+
+
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) { //on launching of this activity
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filters);
 
@@ -43,24 +100,32 @@ public class FiltersActivity extends AppCompatActivity {
         interests = findViewById(R.id.interests);
         zip = findViewById(R.id.zip);
         dispage = findViewById(R.id.dispage);
+        interestL = findViewById(R.id.interestsL);
+        zipL = findViewById(R.id.zipL);
+        dispageL = findViewById(R.id.dispageL);
+        majorL = findViewById(R.id.majorL);
         age.setRangeValues(18, 30);
         majorSpinner.setItems(majorvalues);
+        dispage.setText("18-30");
 
+        initializeTheme();
 
-        //the function called when the cancel button is clicked
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String myinterests = interests.getText().toString();
-                String majors = majorSpinner.getSelectedItemsAsString();
-                String myzip = zip.getText().toString();
-                Toast.makeText(FiltersActivity.this, myinterests + "\n" + myzip + "\n" + min + "-" + max + "\n" + majors, Toast.LENGTH_LONG).show();
+//                String myinterests = interests.getText().toString();
+//                String majors = majorSpinner.getSelectedItemsAsString();
+//                String myzip = zip.getText().toString();
+                //Toast.makeText(FiltersActivity.this, myinterests + "\n" + myzip + "\n" + min + "-" + max + "\n" + majors, Toast.LENGTH_LONG).show();
+                Toast.makeText(FiltersActivity.this, "Cancelled", Toast.LENGTH_LONG).show();
+                InputMethodManager inputManager = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
 
-
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
             }
         });
 
-        //the function called when the submit button is clicked
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,12 +133,15 @@ public class FiltersActivity extends AppCompatActivity {
                 String majors = majorSpinner.getSelectedItemsAsString();
                 String myzip = zip.getText().toString();
                 Toast.makeText(FiltersActivity.this, myinterests + "\n" + myzip + "\n" + min + "-" + max + "\n" + majors, Toast.LENGTH_LONG).show();
+                InputMethodManager inputManager = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
 
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
 
             }
         });
 
-        //called whenever a chnage is made to the age seekbar
         age.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener<Integer>() {
             @Override
             public void onRangeSeekBarValuesChanged(RangeSeekBar<?> bar, Integer minValue, Integer maxValue) {
@@ -84,8 +152,25 @@ public class FiltersActivity extends AppCompatActivity {
             }
         });
 
-        //notifies while the ae seekbar is being dragged
         age.setNotifyWhileDragging(true);
 
     }
+    private void initializeTheme() {
+        String theme = "";
+        try {
+            FileInputStream fis = openFileInput("theme");
+            Scanner scanner = new Scanner(fis);
+            theme = scanner.next();
+            scanner.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (theme.contains("dark"))
+            getAppTheme("dark");
+
+        else
+            getAppTheme("light");
+    }
+
 }
